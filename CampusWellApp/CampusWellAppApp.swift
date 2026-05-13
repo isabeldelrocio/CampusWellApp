5c173e571201a7d645f919d41ffd58a03aab7a66
Isabel Morales <isabeldelrociovmorales@gmail.com>
2026-05-13 00:42:11 -0700

    Initial Commit

---
+++ CampusWellApp/CampusWellAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusWellAppApp.swift
+//  CampusWellApp
+//
+//  Created by isabel on 5/13/26.
+//
+
+import SwiftUI
+
+@main
+struct CampusWellAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
